8ea75d1bcd1c70670c188e6ad75a371352129681
Elix Neto <neto.elix@outlook.com>
2023-09-24 10:02:31 -0300

    Corrigindo lint

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,15 +6,22 @@ function Form({ onCancel }) {
   return (
     <div className="div-form">
       <div>
-        <input type="text" name="" id="service-name" />
+        <input type="text" name="" id="service-name" required />
         <label htmlFor="service-name">Nome do serviço</label>
       </div>
       <div>
-        <input type="text" name="" id="login" />
+        <input type="text" name="" id="login" required />
         <label htmlFor="login">Login</label>
       </div>
       <div>
-        <input type="password" name="" id="password" />
+        <input
+          type="password"
+          name=""
+          id="password"
+          minLength={ 8 }
+          maxLength={ 16 }
+          required
+        />
         <label htmlFor="password">Senha</label>
       </div>
       <div>
